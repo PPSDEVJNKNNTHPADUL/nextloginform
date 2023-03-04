3ef205c6876769dee2b59a6fdf49e9f0b6c95fbb
PPSDEVJNKNNTHPADUL <jklpadul.itd.pps@gmail.com>
2023-03-04 11:20:42 +0800

    swr

---
+++ pages/api/login.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function login(req: NextApiRequest, res: NextApiResponse) {
+  const { username } = req.body;
+  if (!username) {
+    res.status(401);
+    res.json({ message: "Invalid user" });
+  } else {
+    res.setHeader("Set-Cookie", `session=${username};`);
+    res.status(201);
+    res.json({ message: "Logged in" });
+  }
+}
---
+++ pages/api/logout.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function logout(req: NextApiRequest, res: NextApiResponse) {
+if (req.cookies?.session === "invalid") {
+res.status(400);
+res.json({ message: "You are not logged in!" });
+} else {
+res.setHeader("Set-Cookie", "session=invalid;");
+res.status(200);
+res.json({ message: "Logged out" });
+}
+}
